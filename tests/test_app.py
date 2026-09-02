a3312193911479e61e0d2cd99b2fd7f36af02b45
Oscar Torres Quintero <oscartquintero23@gmail.com>
2026-09-02 15:46:19 -0500

    feat: initial DevSecOps API

---
+++ tests/test_app.py
@@ -0,0 +1,19 @@
+from sys import path
+path.append(".")
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json["estado"] == "OK"
+
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json["status"] == "healthy"
